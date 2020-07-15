9a58b19e1525f8a8c3d871bdf3c503e71af56129
Ryxz-JN <joshua.nerling@icloud.com>
2020-07-15 12:42:23 +0200

    LayoutTry

--- resources/views/marks/index.blade.php
+++ resources/views/marks/index.blade.php
@@ -8,7 +8,63 @@
                 <h2>Marks</h2>
             </div>
         </div>
-    </div>
-    
+    </div>    
 </div>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  border-collapse: collapse;
+  border: 1px solid black;
+} 
+
+th,td {
+  border: 1px solid black;
+}
+
+table.a {
+  table-layout: auto;
+  width: 300px;  
+}
+
+</style>
+</head>
+<body>
+    
+<h3>Deutsch</h3>
+<table class="a">
+  <tr>
+    <th>Datum</th>
+    <th>Halbjahr</th>
+    <th>Note</th>
+    <th>Beschreibung</th>
+  </tr>
+  <tr>
+    <td>27.01.2020</td>
+    <td>2.</td>
+    <td>15 NP</td>
+    <td>Klausur</td>
+  </tr>
+
+<h3>Mathe</h3>
+<table class="a">
+  <tr>
+    <th>Datum</th>
+    <th>Halbjahr</th>
+    <th>Note</th>
+    <th>Beschreibung</th>
+  </tr>
+  <tr>
+    <td>27.01.2020</td>
+    <td>2.</td>
+    <td>15 NP</td>
+    <td>Klausur</td>
+  </tr>
+</table>
+
+</body>
+</html>
 @endsection
+
