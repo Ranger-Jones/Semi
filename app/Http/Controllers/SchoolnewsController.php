f265dfa3526c62311402e755522f6a731a8de057
Ryxz-JN <joshua.nerling@icloud.com>
2020-12-19 16:22:54 +0100

    hallo

--- app/Http/Controllers/SchoolnewsController.php
+++ app/Http/Controllers/SchoolnewsController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class SchoolnewsController extends Controller
 {
-    //
+    public function index()
+    {
+        $isTeacher = false;
+        $permissions = auth()->user()->permissions()->get();
+        foreach($permissions as $p){
+            if($p->permission == "Lehrer"){
+                $isTeacher = true;
+            }
+        }
+        return view('schulnews.index', compact("isTeacher"));
+    }
+
+    public function create(){
+        return view('schulnews.create');
+    }
+
+    public function show(){
+        return view('schulnews.show');
+    }
 }
---
+++ resources/views/layouts/startpage.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Startseite</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <!-- Scripts -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/startpage.css') }}" rel="stylesheet">
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+</head>
+<body>
+    <div id="app">
+
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
