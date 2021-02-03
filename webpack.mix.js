4044836a49b4a1775414ab9d6dfe593c9318f41c
Ranger-Jones <jonas.barfknecht@gmail.com>
2021-02-03 16:26:41 +0100

    test

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+    let mix = require('laravel-mix');
+
+mix.js('src/app.js', 'dist').setPublicPath('dist');
